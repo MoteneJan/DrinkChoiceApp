726dc421a8d09c16b209b40110fd1d80acd34d5a
Jan Motene <motenejo@gmail.com>
2023-05-17 11:27:12 +0200

    My commits

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import DrinkChoiceForm from "./DrinkChoiceForm";
 import SavedDecisions from "./SavedDecisions";
 
-
 function App() {
   return (
     <div>
       <h1>Drink Choice</h1>
-      <DrinkChoiceForm onSave={handleSave} />
-      <SavedDecisions />
+      <DrinkChoiceForm />
+      <SavedDecisions onSave={handleSave} />
     </div>
   );
 }
-
 export default App;
 
--- src/SavedDecisions.jsx
+++ src/SavedDecisions.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 
 function SavedDecisions() {
   const [savedData, setSavedData] = useState([]);
